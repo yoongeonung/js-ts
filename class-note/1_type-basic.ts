04efb75b9499b3309db505e3bb3a89689b0d2d8b
yoongeonung <yoongeonung@gmail.com>
2021-10-12 10:07:52 +0900

    feat : TS type definition

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,22 @@
+// js
+let str = 'hello';
+
+// ts
+// string
+let strTS: string = 'hello';
+// number
+let num: number = 10;
+// arr
+let arr: Array<number> = [1, 2, 3];
+let arrStr: Array<string> = ['abc', 'def'];
+let arrNum: number[] = [4, 5, 6];
+// tuple
+let address: [string, number] = ['gangnam', 1010];
+// object
+let capt: { name: string; age: number } = {
+  name: 'Captain America',
+  age: 200,
+};
+let iron: object = {};
+// boolean
+let flag: boolean = true;
---
+++ why-ts/sample.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ * @returns number
+ */
+const sum = (a, b) => {
+  return a + b;
+};
+
+sum(1, 2);
