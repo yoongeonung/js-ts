35210c00ff0aa0c5a0b5c01c1383c7c9143fb29c
yoongeonung <yoongeonung@gmail.com>
2021-10-20 16:08:25 +0900

    Type def of Second Project

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,16 +1,21 @@
-const emails = [
+interface Props<T> {
+  value: T;
+  selected: boolean;
+}
+
+const emails: Props<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts = [
+const numberOfProducts: Props<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
+function createDropdownItem<T>(item: Props<T>): HTMLOptionElement {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -19,8 +24,8 @@ function createDropdownItem(item) {
 }
 
 // NOTE: 이메일 드롭 다운 아이템 추가
-emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+emails.forEach(function (email: Props<string>) {
+  const item = createDropdownItem<string>(email);
   const selectTag = document.querySelector('#email-dropdown');
   selectTag.appendChild(item);
 });
--- quiz/2_address-book/src/index.ts
+++ quiz/2_address-book/src/index.ts
@@ -12,9 +12,9 @@ interface Contact {
 
 // api
 // TODO: 아래 함수의 반환 타입을 지정해보세요.
-function fetchContacts() {
+function fetchContacts(): Promise<Contact[]> {
   // TODO: 아래 변수의 타입을 지정해보세요.
-  const contacts = [
+  const contacts: Contact[] = [
     {
       name: 'Tony',
       address: 'Malibu',
@@ -49,7 +49,7 @@ function fetchContacts() {
       },
     },
   ];
-  return new Promise(resolve => {
+  return new Promise((resolve: Function) => {
     setTimeout(() => resolve(contacts), 2000);
   });
 }
@@ -57,43 +57,45 @@ function fetchContacts() {
 // main
 class AddressBook {
   // TODO: 아래 변수의 타입을 지정해보세요.
-  contacts = [];
+  contacts: Contact[] = [];
 
   constructor() {
     this.fetchData();
   }
 
-  fetchData() {
-    fetchContacts().then(response => {
+  fetchData(): void {
+    fetchContacts().then((response: Contact[]) => {
       this.contacts = response;
     });
   }
 
   /* TODO: 아래 함수들의 파라미터 타입과 반환 타입을 지정해보세요 */
-  findContactByName(name) {
-    return this.contacts.filter(contact => contact.name === name);
+  findContactByName(name: string): Contact[] {
+    return this.contacts.filter((contact: Contact) => contact.name === name);
   }
 
-  findContactByAddress(address) {
-    return this.contacts.filter(contact => contact.address === address);
-  }
-
-  findContactByPhone(phoneNumber, phoneType: string) {
+  findContactByAddress(address: string): Contact[] {
     return this.contacts.filter(
-      contact => contact.phones[phoneType].num === phoneNumber
+      (contact: Contact) => contact.address === address
     );
   }
 
-  addContact(contact) {
+  findContactByPhone(phoneNumber: number, phoneType: string): Contact[] {
+    return this.contacts.filter(
+      (contact: Contact) => contact.phones[phoneType].num === phoneNumber
+    );
+  }
+
+  addContact(contact: Contact): void {
     this.contacts.push(contact);
   }
 
-  displayListByName() {
-    return this.contacts.map(contact => contact.name);
+  displayListByName(): string[] {
+    return this.contacts.map((contact: Contact) => contact.name);
   }
 
-  displayListByAddress() {
-    return this.contacts.map(contact => contact.address);
+  displayListByAddress(): string[] {
+    return this.contacts.map((contact: Contact) => contact.address);
   }
   /* ------------------------------------------------ */
 }
