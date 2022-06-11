9bf1fd8c57b0e03a77065c76bf059042d95bac80
yoongeonung <yoongeonung@gmail.com>
2022-06-11 11:28:09 +0900

    INF : ts types-1

---
+++ ts-for-beginners/getting-started/example/1_type_01.ts
@@ -0,0 +1,11 @@
+// string
+const str: string = "string";
+// number
+const num: number = 10;
+// boolean
+const flag: boolean = true;
+// array
+const arr: Array<string> = [];
+const arr2: [] = [];
+// tuple
+1
