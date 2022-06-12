3a76c4f3bd9cdace54023c5d999618c0ff77b999
yoongeonung <yoongeonung@gmail.com>
2022-06-12 22:52:03 +0900

    ReactMS : styled-component extends component

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,8 +1,25 @@
-const App = () => (
-    <div style={{display: "flex"}}>
-        <div style={{backgroundColor: "teal", width: 100, height: 100}}></div>
-        <div style={{backgroundColor: "tomato", width: 100, height: 100}}></div>
-    </div>
-);
+import styled from "styled-components";
+
+function App() {
+    const Father = styled.div`
+      display: flex;
+    `;
+    const Box = styled.div`
+      background-color: ${props => props.bgColor};
+      width: 100px;
+      height: 100px;
+    `;
+    const Circle = styled(Box)`
+      border-radius: 50px;
+    `;
+
+    return (
+        <Father>
+            <Box bgColor={"green"}/>
+            <Box bgColor={"blueviolet"}/>
+            <Circle bgColor={"whitesmoke"} />
+        </Father>
+    )
+}
 
 export default App;
