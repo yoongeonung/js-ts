167673ebf5f418ec6002017d48a32f0c2e87a8c2
yoongeonung <yoongeonung@gmail.com>
2022-06-13 22:15:17 +0900

    INF : ts - type aliases

---
+++ ts-for-beginners/class-note/4_type_aliases.ts
@@ -0,0 +1,16 @@
+type People = {
+    name: string;
+    age: number
+}
+
+const peopleType: People = {
+  name: "yoon",
+  age: 20,
+};
+console.log(peopleType);
+
+type MyName = string;
+const myName: MyName = "kakao";
+console.log(myName);
+
+
