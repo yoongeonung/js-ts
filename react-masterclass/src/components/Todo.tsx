7a68d3abb8f6678d566473d6bdf3248baba76d6b
yoongeonung <yoongeonung@gmail.com>
2022-06-28 23:55:09 +0900

    feat : Category buttons

--- react-masterclass/src/components/Todo.tsx
+++ react-masterclass/src/components/Todo.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import {ITodo} from "../atoms";
+import {Category, ITodo, toDoState} from "../atoms";
+import {useSetRecoilState} from "recoil";
 
 
-function Todo({text}: ITodo) {
+function Todo({text, category}: ITodo) {
+    const setTodos = useSetRecoilState(toDoState);
+    const onClick = (clickedCategory : Category) => {
+    }
     return (
-        <li>{text}</li>
+        <li>
+            <span>{text}</span>
+            {category !== Category.DOING && <button onClick={() => onClick(Category.DOING)}>Doing</button>}
+            {category !== Category.TODO && <button onClick={() => onClick(Category.TODO)}>To Do</button>}
+            {category !== Category.DONE && <button onClick={() => onClick(Category.DONE)}>Done</button>}
+        </li>
     )
 }
 
